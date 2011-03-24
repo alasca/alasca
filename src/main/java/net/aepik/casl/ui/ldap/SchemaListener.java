7aca2d9ecaabb57ec09995edf7c80be9a2a37789
tchemineau <thomas.chemineau@gmail.com>
2011-03-24 11:42:56 +0000

    # 13 - catch event on rename button + typo
    git-svn-id: http://svn.aepik.net/repo/casl/trunk@111 74b971ab-bbec-4304-98d4-7dc9b701e103

--- src/main/java/net/aepik/casl/ui/ldap/SchemaListener.java
+++ src/main/java/net/aepik/casl/ui/ldap/SchemaListener.java
@@ -1,7 +1,5 @@
 /*
- * SchemaListener.java		0.1		08/06/2006
- * 
- * Copyright (C) 2006 Thomas Chemineau
+ * Copyright (C) 2006-2011 Thomas Chemineau
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,7 +21,6 @@ package net.aepik.casl.ui.ldap;
 
 import net.aepik.casl.core.ldap.Schema;
 import net.aepik.casl.core.ldap.SchemaObject;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -42,95 +39,96 @@ import javax.swing.event.TreeSelectionListener;
  * Cette classe écoute tous les événements qui interviennent sur
  * le modèle et sur la vue. C'est lui qui gère l'intéraction entre
  * la vue du schéma et le modèle de données du schéma.
-**/
+ **/
 
-public class SchemaListener
-		implements
-			ActionListener,
-			MouseListener,
-			Observer,
-			TreeSelectionListener {
+public class SchemaListener implements ActionListener, MouseListener, Observer, TreeSelectionListener
+{
 
-////////////////////////////////
-// Attributs
-////////////////////////////////
+	/**
+	 * Le modèle de données du schéma
+	 */
+	private Schema schema;
 
-	/** Le modèle de données du schéma **/
-	private Schema schema ;
-	/** La vue du schéma **/
-	private SchemaPanel schemaPanel ;
-	/** Le listener du manager de schémas **/
-	private SchemaManagerListener schemaManagerListener ;
-	/** Le chemin du dernier objet sélectionné **/
-	private TreePath lastSelectedPath ;
+	/**
+	 * La vue du schéma
+	 */
+	private SchemaPanel schemaPanel;
 
-////////////////////////////////
-// Constructeurs
-////////////////////////////////
+	/**
+	 * Le listener du manager de schémas
+	 */
+	private SchemaManagerListener schemaManagerListener;
+
+	/**
+	 * Le chemin du dernier objet sélectionné
+	 */
+	private TreePath lastSelectedPath;
 
 	/**
 	 * Créé un nouvel objet SchemaListener.
 	 * @param s Le modèle de données du schéma, un objet Schema.
 	 * @param p La vue du schéma, un objet SchemaPanel.
-	**/
-	public SchemaListener( SchemaManagerListener l, Schema s, SchemaPanel p ) {
-
+	 */
+	public SchemaListener (SchemaManagerListener l, Schema s, SchemaPanel p)
+	{
 		schemaManagerListener = l;
 		schema = s;
 		schemaPanel = p;
-		lastSelectedPath = null ;
-		schema.addObserver( this );
+		lastSelectedPath = null;
+		schema.addObserver(this);
 	}
-
-////////////////////////////////
-// Methodes publiques
-////////////////////////////////
 
 	/**
 	 * Gère les actions de la vue et permet de modifier les données.
 	 * @param e L'action soulevée par un élément du panel.
-	**/
-	public void actionPerformed( ActionEvent e ) {
-
+	 */
+	public void actionPerformed (ActionEvent e)
+	{
 		Object o = e.getSource();
-
-		// Supprimer l'objet sélectionné à l'aide de la souris.
-		// On demande une confirmation.
-		if( o==schemaPanel.item_supprimer ) {
-			if( JOptionPane.showConfirmDialog( schemaPanel,
+		if (o == schemaPanel.item_supprimer)
+		{
+			if (JOptionPane.showConfirmDialog(schemaPanel,
 					"Supprimer cet élément ?", "Confirmation suppression",
 					JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.WARNING_MESSAGE )==JOptionPane.OK_OPTION ) {
-
+					JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION)
+			{
 				schemaPanel.removeCurrentSelectedNode();
 			}
-
-		// On souhaite modifier l'objet sélectionné.
-		// On ouvre une fenêtre d'édition.
-		} else if( o==schemaPanel.item_propriete
-				|| o==schemaPanel.item_propriete2 ) {
-
-			lastSelectedPath = schemaPanel.getCurrentSelectedPath() ;
+		}
+		if (o == schemaPanel.item_propriete || o == schemaPanel.item_propriete2)
+		{
+			lastSelectedPath = schemaPanel.getCurrentSelectedPath();
 			SchemaObjectEditorFrame sof = new SchemaObjectEditorFrame(
 					schemaManagerListener.getManagerFrame(),
 					schema,
-					schemaPanel.getCurrentSelectedObject() );
-			sof.setVisible( true );
+					schemaPanel.getCurrentSelectedObject());
+			sof.setVisible(true);
+		}
+		if (o == schemaPanel.item_rename)
+		{
+			lastSelectedPath = schemaPanel.getCurrentSelectedPath();
+			String result = JOptionPane.showInputDialog(
+				schemaPanel,
+				"Spécifier le nouveau nom pour ce noeud:",
+				"Renommer un noeud",
+				JOptionPane.QUESTION_MESSAGE
+			);
+			if (result != null && result.length() != 0)
+			{
+				System.out.println("rename");
+			}
 		}
 	}
 
 	/**
 	 * Gère l'ensemble des événements de la souris.
 	 * @param e L'événement de la souris.
-	**/
-	public void mouseAction( MouseEvent e ) {
-
-		if( e!=null ) {
-			// Si c'est le cas, on regarde si l'événement de la souris est une
-			// action pour afficher un menu contextuel, suivant la plateforme.
-			if( e.isPopupTrigger() ) {
-				schemaPanel.showPopupMenu( e.getComponent(), e.getX(), e.getY() );
-			}
+	 */
+	public void mouseAction (MouseEvent e)
+	{
+		if (e != null && e.isPopupTrigger())
+		{
+			schemaPanel.showPopupMenu(e.getComponent(), e.getX(), e.getY());
 		}
 	}
 
@@ -163,20 +161,34 @@ public class SchemaListener
 	 * Gère les changement intervenus sur l'arbre. Typiquement, les changements
 	 * sont des sélections d'élement dans l'arbre.
 	 * @param e L'événement soulevée lorsqu'un changement intervient sur l'arbre.
-	**/
-	public void valueChanged( TreeSelectionEvent e ) {
+	 */
+	public void valueChanged (TreeSelectionEvent e)
+	{
 		schemaPanel.updateTable();
 	}
 
-	// Les méthodes liées à la souris.
-	public void mouseClicked( MouseEvent e ) { mouseAction( e ); }
-	public void mouseEntered( MouseEvent e ) {}
-	public void mouseExited( MouseEvent e ) {}
-	public void mousePressed( MouseEvent e ) { mouseAction( e ); }
-	public void mouseReleased( MouseEvent e ) { mouseAction( e ); }
+	public void mouseClicked (MouseEvent e)
+	{
+		mouseAction(e);
+	}
 
-////////////////////////////////
-// Methodes privées
-////////////////////////////////
+	public void mouseEntered (MouseEvent e)
+	{
+	}
+
+	public void mouseExited (MouseEvent e)
+	{
+	}
+
+	public void mousePressed (MouseEvent e)
+	{
+		mouseAction(e);
+	}
+
+	public void mouseReleased (MouseEvent e)
+	{
+		mouseAction(e);
+	}
 
 }
+
