33dc65388da015290446343f9b410faebbd70134
tchemineau <thomas.chemineau@gmail.com>
2011-03-30 14:11:57 +0000

    refactoring code
    git-svn-id: http://svn.aepik.net/repo/casl/trunk@123 74b971ab-bbec-4304-98d4-7dc9b701e103

--- src/main/java/net/aepik/casl/ui/util/LoadFileFrame.java
+++ src/main/java/net/aepik/casl/ui/util/LoadFileFrame.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2010 Thomas Chemineau
+ * Copyright (C) 2006-2011 Thomas Chemineau
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,6 +23,7 @@ import net.aepik.casl.core.ldap.SchemaFile;
 import net.aepik.casl.core.SchemaManager;
 import net.aepik.casl.core.ldap.SchemaSyntax;
 import net.aepik.casl.core.util.Pref;
+import net.aepik.casl.ui.ManagerFrame;
 import net.aepik.casl.ui.util.DescriptiveInternalFrame;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -43,6 +44,8 @@ import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -53,243 +56,296 @@ import javax.swing.table.TableColumn;
 public class LoadFileFrame extends JDialog implements ActionListener, WindowListener
 {
 
-private static final long serialVersionUID = 0;
+	private static final long serialVersionUID = 0;
 
-	/** La fenêtre appelante **/
-	private JFrame mainFrame ;
+	/**
+	 * La fenêtre appelante 
+	 */
+	private JFrame mainFrame;
 
-	/** Le manager de schéma **/
-	private SchemaManager manager ;
+	/**
+	 * Le manager de schéma
+	 */
+	private SchemaManager manager;
 
-	/** Le champs contenant le nom du fichier **/
-	private JTextField filename ;
+	/**
+	 * Le champs contenant le nom du fichier
+	 */
+	private JTextField filename;
 
-	/** La liste contenant l'ensemble des syntaxes disponibles **/
-	private JComboBox syntaxes ;
+	/**
+	 * La liste contenant l'ensemble des syntaxes disponibles
+	 */
+	private JComboBox syntaxes;
 
-	/** Le bouton de choix du fichier **/
-	private JButton boutonOpenFile ;
+	/**
+	 * Le bouton de choix du fichier
+	 */
+	private JButton boutonOpenFile;
 
-	/** Le bouton Ok **/
-	private JButton boutonOk ;
+	/**
+	 * Le bouton Ok
+	 */
+	private JButton boutonOk;
 
-	/** Le bouton Annuler **/
-	private JButton boutonAnnuler ;
+	/**
+	 * Le bouton Annuler
+	 */
+	private JButton boutonAnnuler;
 
-////////////////////////////////
-// Constructeurs
-////////////////////////////////
+	/**
+	 * Contains error message.
+	 */
+	private String errorMessage;
 
-	public LoadFileFrame( JFrame f, SchemaManager m ) {
-
-		super( f, "Ouvrir un fichier Schema", true );
-		setSize( 400, 380 );
-		setResizable( false );
-		setLocationRelativeTo( f );
-
-		mainFrame = f ;
-
-		manager = m;
-		filename = new JTextField();
-		syntaxes = new JComboBox();
-		boutonOpenFile = new JButton( "..." );
-		boutonOk = new JButton( "Charger" );
-		boutonAnnuler = new JButton( "Annuler" );
-
+	/**
+	 * Build new LoadFileFrame object.
+	 */
+	public LoadFileFrame ( JFrame f, SchemaManager m )
+	{
+		super(f, "Ouvrir un fichier Schema", true);
+		this.setSize(400, 380);
+		this.setResizable(false);
+		this.setLocationRelativeTo(f);
+		this.mainFrame = f;
+		this.manager = m;
+		this.filename = new JTextField();
+		this.syntaxes = new JComboBox();
+		this.boutonOpenFile = new JButton("...");
+		this.boutonOk = new JButton("Charger");
+		this.boutonAnnuler = new JButton("Annuler");
 		initFrame();
 	}
 
-////////////////////////////////
-// Methodes publiques
-////////////////////////////////
-
-	public void actionPerformed( ActionEvent e ) {
-
+	/**
+	 * Perform action on event for this object.
+	 */
+	public void actionPerformed ( ActionEvent e )
+	{
 		Object o = e.getSource();
-
-		// On ouvre un naviguateur de fichiers
-		// On récupère le nom du fichier sélectionné.
-		if( o==boutonOpenFile ) {
-
-			JFileChooser jfcProgramme = new JFileChooser( "." );
-			jfcProgramme.setMultiSelectionEnabled( false );
-			jfcProgramme.setDialogTitle( "Selectionner un fichier" );
-			jfcProgramme.setApproveButtonText( "Selectionner" );
-			jfcProgramme.setApproveButtonToolTipText( "Cliquer apres avoir selectionné un fichier" );
+		if (o == boutonOpenFile)
+		{
+			JFileChooser jfcProgramme = new JFileChooser(".");
+			jfcProgramme.setMultiSelectionEnabled(false);
+			jfcProgramme.setDialogTitle("Selectionner un fichier");
+			jfcProgramme.setApproveButtonText("Selectionner");
+			jfcProgramme.setApproveButtonToolTipText("Cliquer apres avoir selectionné un fichier");
 			jfcProgramme.setAcceptAllFileFilterUsed(false);
-
-			if( jfcProgramme.showDialog( this, null )==JFileChooser.APPROVE_OPTION ) {
-
-				try {
-					filename.setText( jfcProgramme.getSelectedFile().getCanonicalPath() );
-				} catch( IOException ioe ) {
-					JOptionPane.showMessageDialog( null, "Erreur de nom de fichier.", "Erreur", JOptionPane.ERROR_MESSAGE );
+			if (jfcProgramme.showDialog(this, null) == JFileChooser.APPROVE_OPTION)
+			{
+				try
+				{
+					filename.setText(jfcProgramme.getSelectedFile().getCanonicalPath());
+				}
+				catch (IOException ioe)
+				{
+					JOptionPane.showMessageDialog(null, "Erreur de nom de fichier.", "Erreur", JOptionPane.ERROR_MESSAGE);
 				}
 			}
-
-		// On récupère toutes les infos importantes : nom de fichier et syntaxe.
-		// On créé dynamiquement une nouvelle instance de la syntaxe.
-		// On ouvre le fichier et on le parcourt avec la syntaxe.
-		// Si le fichier est chargé correctement, on l'ajoute à la vue
-		// principale et on ferme cette fenêtre.
-		} else if( o==boutonOk && filename.getText().length()!=0 ) {
-
-			if( (new File( filename.getText() )).exists() ) {
-
-				try {
-
-					// On créer l'objet de la syntaxe dynamiquement.
-					String syntaxName = (String) syntaxes.getSelectedItem();
-					SchemaSyntax syntax = Schema.getSyntax(syntaxName);
-
-					// On charge.
-					SchemaFile schemaFile = Schema.createAndLoad(syntax, filename.getText(), true);
-					Schema schema = schemaFile.getSchema();
-
-					// Si le chargement du fichier ne réussi pas, le format est incorrect.
-					if( schema==null )
-					{
-						String message = "Le format du fichier est incorrect.";
-						if (schemaFile.isError())
-						{
-							message += "\n\n";
-							message += "Line " + schemaFile.getErrorLine() + ":\n";
-							message += schemaFile.getErrorMessage();
-						}
-						JOptionPane.showMessageDialog( this, message, "Erreur", JOptionPane.ERROR_MESSAGE );
-
-					// Sinon si l'ajout echoue, le fichier est déjà ouvert.
-					} else if( !manager.addSchema( (new File( filename.getText() )).getName(), schema ) ) {
-						JOptionPane.showMessageDialog( this, "Le fichier est déjà ouvert.", "Erreur", JOptionPane.ERROR_MESSAGE );
-
-					// Sinon, ok.
-					}
-					else
-					{
-						Vector<String> files = Pref.getVector(Pref.PREF_LASTOPENFILES);
-						if (!files.contains(filename.getText()))
-						{
-							files.add(filename.getText());
-							if (files.size() > 5)
-							{
-								files.removeElementAt(0);
-							}
-							Pref.set(Pref.PREF_LASTOPENFILES, files.toArray(new String[0]));
-						}
-						windowClosing( null );
-					}
-
-				// Erreur lors de la lecture du fichier.					
-				} catch( Exception ex ) {
-					System.out.println( ex );
-					JOptionPane.showMessageDialog( this, "Une erreur est survenue lors de la lecture du fichier.", "Erreur", JOptionPane.ERROR_MESSAGE );
-				}
-			}
-
-		// On annule, toutes les informations sont perdues.
-		} else if( o==boutonAnnuler ) {
-			windowClosing( null );
 		}
-
+		if (o == boutonOk && filename.getText().length() != 0)
+		{
+			if (!this.loadFile(filename.getText()))
+			{
+				JOptionPane.showMessageDialog(
+					this,
+					this.errorMessage,
+					"Erreur",
+					JOptionPane.ERROR_MESSAGE
+				);
+			}
+			windowClosing(null);
+		}
+		if (o == boutonAnnuler)
+		{
+			windowClosing(null);
+		}
 	}
 
-	public void windowActivated( WindowEvent e ) {}
-	public void windowClosed( WindowEvent e ) {}
- 	public void windowClosing( WindowEvent e ) { setVisible( false ); }
-	public void windowDeactivated( WindowEvent e ) {}
-	public void windowDeiconified( WindowEvent e ) {}
-	public void windowIconified( WindowEvent e ) {}
-	public void windowOpened( WindowEvent e ) {}
-
-////////////////////////////////
-// Methodes privées
-////////////////////////////////
-
-	private void initFrame() {
-
+	/**
+	 * Initialize frame content.
+	 */
+	private void initFrame ()
+	{
 		// - Panel bouton du bas -
 
-		JPanel boutonsPanel = new JPanel( new FlowLayout( FlowLayout.RIGHT ) );
-		boutonsPanel.add( boutonOk );
-		boutonsPanel.add( boutonAnnuler );
+		JPanel boutonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		boutonsPanel.add(boutonOk);
+		boutonsPanel.add(boutonAnnuler);
 
 		// - Panel nom du fichier -
 
 		JTextArea textAreaFilename = new JTextArea(
-				"Indiquez le nom du fichier contenant les définitions du schéma LDAP que vous"
-				+ " souhaitez charger." );
-		textAreaFilename.setEditable( false );
-		textAreaFilename.setLineWrap( true );
-		textAreaFilename.setWrapStyleWord( true );
-		textAreaFilename.setFont( (new JLabel()).getFont() );
-		textAreaFilename.setBorder( BorderFactory.createEmptyBorder( 7, 6, 12, 6 ) );
-		textAreaFilename.setBackground( new Color(240, 235, 226) );
+			"Indiquez le nom du fichier contenant les définitions du schéma LDAP que vous souhaitez charger."
+		);
+		textAreaFilename.setEditable(false);
+		textAreaFilename.setLineWrap(true);
+		textAreaFilename.setWrapStyleWord(true);
+		textAreaFilename.setFont((new JLabel()).getFont());
+		textAreaFilename.setBorder(BorderFactory.createEmptyBorder(7, 6, 12, 6));
+		textAreaFilename.setBackground(new Color(240, 235, 226));
 
-		boutonOpenFile.setBorder( BorderFactory.createCompoundBorder(
-				BorderFactory.createMatteBorder( 0, 5, 0, 0, boutonsPanel.getBackground() ),
-				boutonOpenFile.getBorder() ) );
+		boutonOpenFile.setBorder(
+			BorderFactory.createCompoundBorder(
+				BorderFactory.createMatteBorder(0, 5, 0, 0, boutonsPanel.getBackground()),
+				boutonOpenFile.getBorder()
+			)
+		);
 
-		JPanel filenamePanel = new JPanel( new BorderLayout() );
-		filenamePanel.add( textAreaFilename, BorderLayout.NORTH );
-		filenamePanel.add( filename, BorderLayout.CENTER );
-		filenamePanel.add( boutonOpenFile, BorderLayout.EAST );
-		filenamePanel.setBorder( BorderFactory.createCompoundBorder(
-				BorderFactory.createEmptyBorder( 5, 4, 1, 4 ),
-				BorderFactory.createCompoundBorder( 
-					BorderFactory.createTitledBorder( " Schéma LDAP " ),
-					BorderFactory.createEmptyBorder( 0, 5, 5, 5 ) ) ) );
+		JPanel filenamePanel = new JPanel(new BorderLayout());
+		filenamePanel.add(textAreaFilename, BorderLayout.NORTH);
+		filenamePanel.add(filename, BorderLayout.CENTER);
+		filenamePanel.add(boutonOpenFile, BorderLayout.EAST);
+		filenamePanel.setBorder(
+			BorderFactory.createCompoundBorder(
+				BorderFactory.createEmptyBorder(5, 4, 1, 4),
+				BorderFactory.createCompoundBorder(
+					BorderFactory.createTitledBorder(" Schéma LDAP "),
+					BorderFactory.createEmptyBorder(0, 5, 5, 5 )
+				)
+			)
+		);
 
 		// - Panel du selecteur de syntaxes -
 
 		JTextArea textAreaSyntaxes = new JTextArea(
-				"Il vous faut appliquer un filtre sur le schéma que vous voulez charger."
-				+ " Voici la liste des filtres disponibles :" );
-		textAreaSyntaxes.setEditable( false );
-		textAreaSyntaxes.setLineWrap( true );
-		textAreaSyntaxes.setWrapStyleWord( true );
-		textAreaSyntaxes.setFont( (new JLabel()).getFont() );
-		textAreaSyntaxes.setBorder( BorderFactory.createEmptyBorder( 7, 6, 12, 6 ) );
-		textAreaSyntaxes.setBackground( new Color(240, 235, 226) );
+			"Il vous faut appliquer un filtre sur le schéma que vous voulez charger. Voici la liste des filtres disponibles :"
+		);
+		textAreaSyntaxes.setEditable(false);
+		textAreaSyntaxes.setLineWrap(true);
+		textAreaSyntaxes.setWrapStyleWord(true);
+		textAreaSyntaxes.setFont((new JLabel()).getFont());
+		textAreaSyntaxes.setBorder(BorderFactory.createEmptyBorder(7, 6, 12, 6));
+		textAreaSyntaxes.setBackground(new Color(240, 235, 226));
 
-		JPanel syntaxesPanel = new JPanel( new BorderLayout() );
-		syntaxesPanel.add( textAreaSyntaxes, BorderLayout.NORTH );
-		syntaxesPanel.add( syntaxes );
-		syntaxesPanel.setBorder( BorderFactory.createCompoundBorder(
-				BorderFactory.createEmptyBorder( 5, 4, 1, 4 ),
-				BorderFactory.createCompoundBorder( 
-					BorderFactory.createTitledBorder( " Syntaxe LDAP " ),
-					BorderFactory.createEmptyBorder( 0, 5, 5, 5 ) ) ) );
+		JPanel syntaxesPanel = new JPanel(new BorderLayout());
+		syntaxesPanel.add(textAreaSyntaxes, BorderLayout.NORTH);
+		syntaxesPanel.add(syntaxes);
+		syntaxesPanel.setBorder(
+			BorderFactory.createCompoundBorder(
+				BorderFactory.createEmptyBorder(5, 4, 1, 4),
+				BorderFactory.createCompoundBorder(
+					BorderFactory.createTitledBorder(" Syntaxe LDAP "),
+					BorderFactory.createEmptyBorder(0, 5, 5, 5)
+				)
+			)
+		);
 
 		// On injecte le nom des classes de syntaxes possibles
 		// dynamiquement.
 		String[] syntaxesName = Schema.getSyntaxeNames();
-		for( int i=0; syntaxesName!=null && i<syntaxesName.length; i++ ) {
-			syntaxes.addItem( syntaxesName[i] );
+		for (int i = 0; syntaxesName != null && i < syntaxesName.length; i++)
+		{
+			syntaxes.addItem(syntaxesName[i]);
 		}
 
 		// - Organisation générale -
 
-		JPanel mainPanel = new JPanel( new BorderLayout() );
-		mainPanel.add( filenamePanel, BorderLayout.NORTH );
-		mainPanel.add( syntaxesPanel, BorderLayout.CENTER );
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		mainPanel.add(filenamePanel, BorderLayout.NORTH);
+		mainPanel.add(syntaxesPanel, BorderLayout.CENTER);
 
-		JPanel mainPanelContainer = new JPanel( new BorderLayout() );
-		mainPanelContainer.add( mainPanel, BorderLayout.NORTH );
-		mainPanelContainer.add( boutonsPanel, BorderLayout.SOUTH );
-		mainPanelContainer.setBorder( BorderFactory.createEmptyBorder( 2, 1, 1, 1 ) );
+		JPanel mainPanelContainer = new JPanel(new BorderLayout());
+		mainPanelContainer.add(mainPanel, BorderLayout.NORTH);
+		mainPanelContainer.add(boutonsPanel, BorderLayout.SOUTH);
+		mainPanelContainer.setBorder(BorderFactory.createEmptyBorder(2, 1, 1, 1));
 
-		getContentPane().add( new DescriptiveInternalFrame(
+		getContentPane().add(
+			new DescriptiveInternalFrame(
 				mainFrame.getIconImage(),
 				"Sélectionner un fichier et sa syntaxe.",
-				mainPanelContainer ) );
+				mainPanelContainer
+			)
+		);
 
 		// - Listeners -
 
-		addWindowListener( this );
-		boutonOpenFile.addActionListener( this );
-		boutonOk.addActionListener( this );
-		boutonAnnuler.addActionListener( this );
-
+		addWindowListener(this);
+		boutonOpenFile.addActionListener(this);
+		boutonOk.addActionListener(this);
+		boutonAnnuler.addActionListener(this);
 	}
 
+
+	/**
+	 * Load a file.
+	 */
+	public boolean loadFile ( String filename )
+	{
+		if (!(new File(filename)).exists())
+		{
+			this.errorMessage = "Le fichier n'existe pas.";
+			return false;
+		}
+		try
+		{
+			String syntaxName = (String) syntaxes.getSelectedItem();
+			SchemaSyntax syntax = Schema.getSyntax(syntaxName);
+			SchemaFile schemaFile = Schema.createAndLoad(syntax, filename, true);
+			Schema schema = schemaFile.getSchema();
+			if (schema == null)
+			{
+				String message = "";
+				if (schemaFile.isError())
+				{
+					message += "\n\n"
+						+  "Line " + schemaFile.getErrorLine() + ":\n"
+						+  schemaFile.getErrorMessage();
+				}
+				this.errorMessage = "Le format du fichier est incorrect." + message;
+				return false;
+			}
+			if (!manager.addSchema((new File(filename)).getName(), schema))
+			{
+				this.errorMessage = "Le fichier est déjà ouvert.";
+				return false;
+			}
+			Vector<String> files = Pref.getVector(Pref.PREF_LASTOPENFILES);
+			if (!files.contains(filename))
+			{
+				files.add(filename);
+				if (files.size() > 10)
+				{
+					files.removeElementAt(0);
+				}
+				Pref.set(Pref.PREF_LASTOPENFILES, files.toArray(new String[0]));
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			this.errorMessage = "Une erreur inattendue est survenue.";
+			return false;
+		}
+		return true;
+	}
+
+	public void windowActivated (WindowEvent e)
+	{
+	}
+
+	public void windowClosed ( WindowEvent e )
+	{
+	}
+
+ 	public void windowClosing ( WindowEvent e )
+	{
+		this.setVisible(false);
+	}
+
+	public void windowDeactivated (WindowEvent e)
+	{
+	}
+
+	public void windowDeiconified ( WindowEvent e )
+	{
+	}
+
+	public void windowIconified ( WindowEvent e )
+	{
+	}
+
+	public void windowOpened ( WindowEvent e )
+	{
+	}
 }
